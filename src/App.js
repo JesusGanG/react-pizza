79fb5bb5350512879f2a7192d4eee6140617d9a0
JesusGanG <vip.almosov@mail.ru>
2023-12-01 09:17:47 +0300

    connecting backend with data to the project

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import Categories from './components/Categories'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock'
 
-import pizzas from './assets/pizzas.json'
-
 function App() {
+  const [items, setItems] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://65697001de53105b0dd70719.mockapi.io/items')
+      .then((res) => {
+        return res.json()
+      })
+      .then((arr) => {
+        setItems(arr)
+      })
+  }, [])
+
   return (
     <div className='wrapper'>
       <Header />
@@ -19,9 +29,8 @@ function App() {
           </div>
           <h2 className='content__title'>Все пиццы</h2>
           <div className='content__items'>
-            {pizzas.map((objPizza) => (
-							<PizzaBlock key={objPizza.id} {...objPizza}
-              />
+            {items.map((objPizza) => (
+              <PizzaBlock key={objPizza.id} {...objPizza} />
             ))}
           </div>
         </div>
